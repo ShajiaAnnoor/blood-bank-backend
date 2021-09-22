b8056e733dbf44866772ec6e4e439aa6acf629fb
Tarif Ezaz <tarif.ezaz@gmail.com>
2021-09-22 23:17:31 +0600

    Add package for handling cache

---
+++ cache/redis/connection_status.go
@@ -0,0 +1,55 @@
+package redis
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+	"gitlab.com/Aubichol/hrishi-backend/cache"
+	"gitlab.com/Aubichol/hrishi-backend/cfg"
+	"gitlab.com/Aubichol/hrishi-backend/pkg/tag"
+)
+
+const (
+	connectionStatusPrefix string = "connection_status"
+)
+
+type connectionStatus struct {
+	c   *redis.Client
+	cfg cfg.ConnectionCache
+}
+
+func (c *connectionStatus) key(id1, id2 string) string {
+	return fmt.Sprintf("%s_%s", connectionStatusPrefix, tag.Unique(id1, id2))
+}
+
+func (c *connectionStatus) Has(id1, id2 string) (bool, error) {
+	result := c.c.Get(c.key(id1, id2))
+	if err := result.Err(); err != nil {
+		return false, err
+	}
+
+	v, err := result.Int()
+	if err != nil {
+		return false, err
+	}
+
+	return v == 1, nil
+}
+
+func (c *connectionStatus) Set(id1, id2 string, status bool) error {
+	v := 1
+	if !status {
+		v = 0
+	}
+
+	result := c.c.Set(c.key(id1, id2), v, c.cfg.Length)
+	if err := result.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewConnectionStatus(c *redis.Client, cfg cfg.ConnectionCache) cache.ConnectionStatus {
+	return &connectionStatus{c, cfg}
+}
---
+++ cache/redis/session.go
@@ -0,0 +1,66 @@
+package redis
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+	"github.com/google/uuid"
+	"gitlab.com/Aubichol/hrishi-backend/cache"
+	"gitlab.com/Aubichol/hrishi-backend/model"
+)
+
+const (
+	sessionPrefix string = "session"
+)
+
+type session struct {
+	c *redis.Client
+}
+
+func (s *session) Create(data *model.Session) error {
+	data.Key = fmt.Sprintf("%s_%s_%s", sessionPrefix, data.UserID, uuid.New().String())
+	duration := data.ExpiredAt.Sub(data.CreatedAt)
+	bytes, err := data.ToByte()
+
+	if err != nil {
+		return err
+	}
+
+	result := s.c.Set(data.Key, bytes, duration)
+	return result.Err()
+}
+
+func (s *session) GetByKey(key string) (*model.Session, error) {
+	result := s.c.Get(key)
+	if result.Err() == redis.Nil {
+		return nil, nil
+	}
+
+	if result.Err() != nil {
+		return nil, result.Err()
+	}
+
+	data := model.Session{}
+	bytes, err := result.Bytes()
+	if err != nil {
+		return nil, err
+	}
+
+	if err = data.FromBytes(bytes); err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
+
+func (s *session) GetByUserID(userID string) ([]*model.Session, error) {
+	return nil, nil
+}
+
+func (s *session) RemoveByKey(id string) error {
+	return nil
+}
+
+func NewSession(c *redis.Client) cache.Session {
+	return &session{c}
+}
