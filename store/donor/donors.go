14163fbafbc668ec25612cfb5149150afbf98a5f
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-02-10 22:51:26 +0600

    Update store

--- store/donor/donors.go
+++ store/donor/donors.go
@@ -1,13 +1,13 @@
-package comment
+package donor
 
 import "gitlab.com/Aubichol/blood-bank-backend/model"
 
-// Comments wraps user's comment functionality
-type Comments interface {
-	Save(*model.Comment) (id string, err error)
-	FindByID(id string) (*model.Comment, error)
-	FindByStatusID(id string, skip int64, limit int64) ([]*model.Comment, error)
-	CountByStatusID(id string) (int64, error)
-	FindByIDs(id ...string) ([]*model.Comment, error)
-	Search(q string, skip, limit int64) ([]*model.Comment, error)
+// Donors wraps user's comment functionality
+type Donors interface {
+	Save(*model.Donor) (id string, err error)
+	FindByID(id string) (*model.Donor, error)
+	FindByDonorID(id string, skip int64, limit int64) ([]*model.Donor, error)
+	CountByDonorID(id string) (int64, error)
+	FindByIDs(id ...string) ([]*model.Donor, error)
+	Search(q string, skip, limit int64) ([]*model.Donor, error)
 }
--- store/donor/mongo/donors.go
+++ store/donor/mongo/donors.go
@@ -5,8 +5,7 @@ import (
 	"fmt"
 
 	"gitlab.com/Aubichol/blood-bank-backend/model"
-	storecomment "gitlab.com/Aubichol/blood-bank-backend/store/comment"
-	mongoModel "gitlab.com/Aubichol/blood-bank-backend/store/comment/mongo/model"
+	mongoModel "gitlab.com/Aubichol/blood-bank-backend/store/donor/mongo/model"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -15,33 +14,33 @@ import (
 )
 
 //users handles user related database queries
-type comments struct {
+type donors struct {
 	c *mongo.Collection
 }
 
-func (c *comments) convertData(modelComment *model.Comment) (
-	mongoComment mongoModel.Comment,
+func (c *donors) convertData(modelDonor *model.Donor) (
+	mongoDonor mongoModel.Donor,
 	err error,
 ) {
-	err = mongoComment.FromModel(modelComment)
+	err = mongoDonor.FromModel(modelDonor)
 	return
 }
 
 // Save saves comments from model to database
-func (c *comments) Save(modelComment *model.Comment) (string, error) {
-	mongoComment := mongoModel.Comment{}
+func (c *donors) Save(modelDonor *model.Donor) (string, error) {
+	mongoDonor := mongoModel.Donor{}
 	var err error
-	mongoComment, err = c.convertData(modelComment)
+	mongoDonor, err = c.convertData(modelDonor)
 	if err != nil {
 		return "", fmt.Errorf("Could not convert model comment to mongo comment: %w", err)
 	}
 
-	if modelComment.ID == "" {
-		mongoComment.ID = primitive.NewObjectID()
+	if modelDonor.ID == "" {
+		mongoDonor.ID = primitive.NewObjectID()
 	}
 
-	filter := bson.M{"_id": mongoComment.ID}
-	update := bson.M{"$set": mongoComment}
+	filter := bson.M{"_id": mongoDonor.ID}
+	update := bson.M{"$set": mongoDonor}
 	upsert := true
 
 	_, err = c.c.UpdateOne(
@@ -53,32 +52,36 @@ func (c *comments) Save(modelComment *model.Comment) (string, error) {
 		},
 	)
 
-	return mongoComment.ID.Hex(), err
+	return mongoDonor.ID.Hex(), err
 }
 
 //FindByID finds a comment by id
-func (c *comments) FindByID(id string) (*model.Comment, error) {
+func (c *donors) FindByID(id string) (*model.Donor, error) {
 	objectID, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
 		return nil, fmt.Errorf("Invalid id %s : %w", id, err)
 	}
 
 	filter := bson.M{"_id": objectID}
-	result := c.c.FindOne(context.Background(), filter, &options.FindOneOptions{})
+	result := c.c.FindOne(
+		context.Background(),
+		filter,
+		&options.FindOneOptions{},
+	)
 	if err := result.Err(); err != nil {
 		return nil, err
 	}
 
-	comment := mongoModel.Comment{}
-	if err := result.Decode(&comment); err != nil {
+	donor := mongoModel.Donor{}
+	if err := result.Decode(&donor); err != nil {
 		return nil, fmt.Errorf("Could not decode mongo model to model : %w", err)
 	}
 
-	return comment.ModelComment(), nil
+	return donor.ModelDonor(), nil
 }
 
 //FindByStatusID finds a comment by status id
-func (c *comments) FindByStatusID(id string, skip int64, limit int64) ([]*model.Comment, error) {
+func (c *donors) FindByDonorID(id string, skip int64, limit int64) ([]*model.Donor, error) {
 	objectID, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
 		return nil, fmt.Errorf("Invalid id %s : %w", id, err)
@@ -97,11 +100,11 @@ func (c *comments) FindByStatusID(id string, skip int64, limit int64) ([]*model.
 		return nil, err
 	}
 
-	return c.cursorToComments(cursor)
+	return c.cursorToDonors(cursor)
 }
 
 //CountByStatusID returns comments from status id
-func (c *comments) CountByStatusID(id string) (int64, error) {
+func (c *donors) CountByStatusID(id string) (int64, error) {
 	objectID, err := primitive.ObjectIDFromHex(id)
 
 	if err != nil {
@@ -119,7 +122,7 @@ func (c *comments) CountByStatusID(id string) (int64, error) {
 }
 
 //FindByIDs returns all the users from multiple user ids
-func (c *comments) FindByIDs(ids ...string) ([]*model.Comment, error) {
+func (c *donors) FindByIDs(ids ...string) ([]*model.Comment, error) {
 	objectIDs := []primitive.ObjectID{}
 	for _, id := range ids {
 		objectID, err := primitive.ObjectIDFromHex(id)
@@ -145,7 +148,7 @@ func (c *comments) FindByIDs(ids ...string) ([]*model.Comment, error) {
 }
 
 //Search search for users given the text, skip and limit
-func (c *comments) Search(text string, skip, limit int64) ([]*model.Comment, error) {
+func (c *donors) Search(text string, skip, limit int64) ([]*model.Comment, error) {
 	filter := bson.M{"$text": bson.M{"$search": text}}
 	cursor, err := c.c.Find(context.Background(), filter, &options.FindOptions{
 		Skip:  &skip,
@@ -155,33 +158,33 @@ func (c *comments) Search(text string, skip, limit int64) ([]*model.Comment, err
 		return nil, err
 	}
 
-	return c.cursorToComments(cursor)
+	return c.cursorToDonors(cursor)
 }
 
 //cursorToComments decodes users one by one from the search result
-func (c *comments) cursorToComments(cursor *mongo.Cursor) ([]*model.Comment, error) {
+func (c *donors) cursorToDonors(cursor *mongo.Cursor) ([]*model.Donor, error) {
 	defer cursor.Close(context.Background())
-	modelComments := []*model.Comment{}
+	modelDonors := []*model.Donor{}
 
 	for cursor.Next(context.Background()) {
-		comment := mongoModel.Comment{}
-		if err := cursor.Decode(&comment); err != nil {
+		donor := mongoModel.Comment{}
+		if err := cursor.Decode(&donor); err != nil {
 			return nil, fmt.Errorf("Could not decode data from mongo %w", err)
 		}
 
-		modelComments = append(modelComments, comment.ModelComment())
+		modelDonors = append(modelDonors, donor.ModelDonor())
 	}
 
-	return modelComments, nil
+	return modelDonors, nil
 }
 
-//CommentsParams provides parameters for comment specific Collection
-type CommentsParams struct {
+//DonorsParams provides parameters for comment specific Collection
+type DonorsParams struct {
 	dig.In
 	Collection *mongo.Collection `name:"comments"`
 }
 
 //Store provides store for comments
-func Store(params CommentsParams) storecomment.Comments {
-	return &comments{params.Collection}
+func Store(params DonorsParams) storecomment.Donors {
+	return &donors{params.Collection}
 }
