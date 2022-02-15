e978897be973741b6b3e7b3b4553c3f385044732
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-02-15 16:32:42 +0600

    Add comments

--- store/staticcontent/staticcontents.go
+++ store/staticcontent/staticcontents.go
@@ -2,7 +2,7 @@ package staticcontent
 
 import "gitlab.com/Aubichol/blood-bank-backend/model"
 
-// StaticContents wraps user's comment functionality
+// StaticContents wraps static contents functionality
 type StaticContents interface {
 	Save(*model.StaticContent) (id string, err error)
 	FindByID(id string) (*model.StaticContent, error)
