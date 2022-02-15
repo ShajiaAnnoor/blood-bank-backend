3312d1a5a58345e26204a0b34e26b5add8919c31
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-02-15 16:12:00 +0600

    Add model for blood request

--- store/blood-request/blood-requests.go
+++ store/blood-request/blood-requests.go
@@ -1,13 +1,13 @@
-package comment
+package blood-request
 
 import "gitlab.com/Aubichol/blood-bank-backend/model"
 
 // Comments wraps user's comment functionality
-type Comments interface {
-	Save(*model.Comment) (id string, err error)
-	FindByID(id string) (*model.Comment, error)
-	FindByStatusID(id string, skip int64, limit int64) ([]*model.Comment, error)
-	CountByStatusID(id string) (int64, error)
-	FindByIDs(id ...string) ([]*model.Comment, error)
-	Search(q string, skip, limit int64) ([]*model.Comment, error)
+type BloodRequests interface {
+	Save(*model.BloodRequest) (id string, err error)
+	FindByID(id string) (*model.BloodRequest, error)
+	FindByBloodRequestID(id string, skip int64, limit int64) ([]*model.BloodRequest, error)
+	CountByBloodRequestID(id string) (int64, error)
+	FindByIDs(id ...string) ([]*model.BloodRequest, error)
+	Search(q string, skip, limit int64) ([]*model.BloodRequest, error)
 }
