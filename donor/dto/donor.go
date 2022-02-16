0ced01c4491b1ff387a5ba480ff7c7de8d63314e
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-02-16 14:24:33 +0600

    Add data type for donor

---
+++ donor/dto/donor.go
@@ -0,0 +1,50 @@
+package dto
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"gitlab.com/Aubichol/donor-bank-backend/errors"
+	"gopkg.in/go-playground/validator.v9"
+)
+
+// Donor provides dto for donor request
+type Donor struct {
+	ID           string `json:"donor_request_id"`
+	Name         string `json:"name"`
+	Phone        string `json:"phone_number"`
+	District     string `json:"district"`
+	BloodGroup   string `json:"blood_group"`
+	Address      string `json:"address"`
+	Availability bool   `json:"availability"`
+	Status       string `json:"status"`
+	TimesDonated int    `json:"times_donated"`
+	UserID       string `json:"user_id"`
+}
+
+//Validate validates donor request data
+func (c *Donor) Validate(validate *validator.Validate) error {
+	if err := validate.Struct(c); err != nil {
+		return fmt.Errorf(
+			"%s:%w",
+			err.Error(),
+			&errors.Invalid{
+				errors.Base{"invalid data for donor", false},
+			},
+		)
+	}
+	return nil
+}
+
+//FromReader reads donor request from request body
+func (c *Donor) FromReader(reader io.Reader) error {
+	err := json.NewDecoder(reader).Decode(c)
+	if err != nil {
+		return fmt.Errorf("%s:%w", err.Error(), &errors.Invalid{
+			Base: errors.Base{"invalid donor data", false},
+		})
+	}
+
+	return nil
+}
