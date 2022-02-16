e4fe6314cc7e845aef94cada4a7d8be825d1f00b
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-02-16 20:58:04 +0600

    Update type

--- notice/dto/readresp.go
+++ notice/dto/readresp.go
@@ -2,14 +2,14 @@ package dto
 
 import "gitlab.com/Aubichol/hrishi-backend/model"
 
-//ReadResp holds the response data for reading comment
+//ReadResp holds the response data for reading notice
 type ReadResp struct {
-	Comment string `json:"comment"`
-	Sender  string `json:"sender"`
+	Notice string `json:"notice"`
+	Sender string `json:"sender"`
 }
 
 //FromModel converts the model data to response data
-func (r *ReadResp) FromModel(comment *model.Comment) {
-	r.Comment = comment.Comment
-	r.Sender = comment.UserID
+func (r *ReadResp) FromModel(notice *model.Notice) {
+	r.Notice = notice.Notice
+	r.Sender = notice.UserID
 }
