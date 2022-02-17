a2a710a839acfc9ea98525898efd9a397b206d12
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-02-17 15:28:54 +0600

    Refactor names

--- api/notice/read.go
+++ api/notice/read.go
@@ -8,7 +8,8 @@ import (
 	"gitlab.com/Aubichol/blood-bank-backend/api/middleware"
 	"gitlab.com/Aubichol/blood-bank-backend/api/routeutils"
 	"gitlab.com/Aubichol/blood-bank-backend/apipattern"
-	"gitlab.com/Aubichol/blood-bank-backend/comment/dto"
+	"gitlab.com/Aubichol/blood-bank-backend/notice"
+	"gitlab.com/Aubichol/blood-bank-backend/notice/dto"
 	"go.uber.org/dig"
 )
 
