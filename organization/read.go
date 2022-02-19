aa575588798971e5aa9b8d232c4edf0aa0609610
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-02-19 21:43:41 +0600

    Remove redundant code

--- organization/read.go
+++ organization/read.go
@@ -5,7 +5,6 @@ import (
 	"gitlab.com/Aubichol/blood-bank-backend/errors"
 	"gitlab.com/Aubichol/blood-bank-backend/model"
 	"gitlab.com/Aubichol/blood-bank-backend/organization/dto"
-	"gitlab.com/Aubichol/blood-bank-backend/pkg/tag"
 	storeorganization "gitlab.com/Aubichol/blood-bank-backend/store/organization"
 	"go.uber.org/dig"
 )
@@ -18,7 +17,6 @@ type Reader interface {
 //organizationReader implements Reader interface
 type organizationReader struct {
 	organizationes organization.Notice
-	friends        friendrequest.FriendRequests
 }
 
 func (read *organizationReader) askStore(organizationID string) (
@@ -54,15 +52,6 @@ func (read *organizationReader) isSameUser(giverID, userID string) (
 	return
 }
 
-func (read *organizationReader) checkFriendShip(giverID, userID string) (
-	currentRequest *model.FriendRequest,
-	err error,
-) {
-	uniqueTag := tag.Unique(giverID, userID)
-	currentRequest, err = read.friends.FindByUniqueTag(uniqueTag)
-	return
-}
-
 func (read *organizationReader) Read(organizationReq *dto.ReadReq) (*dto.ReadResp, error) {
 	//TO-DO: some validation on the input data is required
 	organization, err := read.askStore(organizationReq.OrganizationID)
@@ -80,19 +69,6 @@ func (read *organizationReader) Read(organizationReq *dto.ReadReq) (*dto.ReadRes
 		return &resp, nil
 	}
 
-	currentRequest, err := read.checkFriendShip(
-		giverID,
-		organizationReq.UserID,
-	)
-	if err != nil {
-		logrus.Error("Could not find friendship error : ", err)
-		return nil, read.giveError()
-	}
-
-	if currentRequest.Organization != "accepted" {
-		return nil, err
-	}
-
 	return &resp, nil
 }
 
@@ -100,13 +76,11 @@ func (read *organizationReader) Read(organizationReq *dto.ReadReq) (*dto.ReadRes
 type NewReaderParams struct {
 	dig.In
 	Organization storeorganization.Organization
-	Friends      friendrequest.FriendRequests
 }
 
 //NewReader provides Reader
 func NewReader(params NewReaderParams) Reader {
 	return &organizationReader{
 		organizationes: params.Organization,
-		friends:        params.Friends,
 	}
 }
