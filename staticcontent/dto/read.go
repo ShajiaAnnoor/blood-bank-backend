c77d58f294572a40a41c211d9039049209a3d7c1
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-02-19 21:48:31 +0600

    Update comments

--- staticcontent/dto/read.go
+++ staticcontent/dto/read.go
@@ -1,7 +1,7 @@
 package dto
 
-//ReadReq stores comment read request data
+//ReadReq stores static content read request data
 type ReadReq struct {
-	UserID    string
-	CommentID string
+	UserID          string
+	StaticContentID string
 }
