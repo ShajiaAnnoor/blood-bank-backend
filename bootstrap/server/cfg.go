f1c7bb510924ad0ea755480a9604b2c2dbad0ba5
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-02-20 18:01:15 +0600

    Update comments

--- bootstrap/server/cfg.go
+++ bootstrap/server/cfg.go
@@ -1,11 +1,26 @@
 package server
 
 import (
-	"gitlab.com/Aubichol/blood-bank-backend/cache/redis"
 	"gitlab.com/Aubichol/blood-bank-backend/container"
 )
 
-func Cache(c container.Container) {
-	c.Register(redis.NewSession)
-	c.Register(redis.NewConnectionStatus)
+//Cfg registers configuration related providers
+func Cfg(c container.Container) {
+	//LoadSession provides a constructor to dig container that loads the duration of a session and the maximum number of sessions a user can have
+	c.Register(cfg.LoadSession)
+	//LoadMongo provides a container that loads mongodb server url name and the name of the database
+	c.Register(cfg.LoadMongo)
+	//LoadRedis provides a contructor to dig container for loading redis related configurations
+	c.Register(cfg.LoadRedis)
+	//LoadServer provides a consturctor to dig container for loading go server related configurations
+	c.Register(cfg.LoadServer)
+	//LoadConnectionCache provides a constructor to dig container for connection cache
+	//TODO: What is connection cache?
+	c.Register(cfg.LoadConnectionCache)
+	//LoadLimit provides a constructor to dig container for limit related data
+	c.Register(cfg.LoadLimit)
+	//LoadWSClient provides a construction to dig container for Web Socket client
+	c.Register(cfg.LoadWSClient)
+	//LoadFile provides a constructor to dig container for maintaining file
+	c.Register(cfg.LoadFile)
 }
