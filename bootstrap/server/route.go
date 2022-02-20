d1523a42f90ece21f74689c5457f1814270b5de6
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-02-20 13:52:57 +0600

    Update comments

---
+++ bootstrap/server/route.go
@@ -0,0 +1,48 @@
+package server
+
+import (
+	"gitlab.com/Aubichol/blood-bank-backend/api/bloodrequest"
+	"gitlab.com/Aubichol/blood-bank-backend/api/donor"
+	"gitlab.com/Aubichol/blood-bank-backend/api/notice"
+	"gitlab.com/Aubichol/blood-bank-backend/api/organization"
+	"gitlab.com/Aubichol/blood-bank-backend/api/patient"
+	"gitlab.com/Aubichol/blood-bank-backend/api/staticcontent"
+
+	"gitlab.com/Aubichol/blood-bank-backend/api/wsroute"
+	"gitlab.com/Aubichol/blood-bank-backend/container"
+)
+
+//Route registers all the route providers to container
+func Route(c container.Container) {
+	c.RegisterGroup(bloodrequest.CreateRoute, "route")
+	c.RegisterGroup(bloodrequest.ReadRoute, "route")
+	c.RegisterGroup(bloodrequest.UpdateRoute, "route")
+	c.RegisterGroup(bloodrequest.DeleteRoute, "route")
+
+	c.RegisterGroup(donor.CreateRoute, "route")
+	c.RegisterGroup(donor.ReadRoute, "route")
+	c.RegisterGroup(donor.UpdateRoute, "route")
+	c.RegisterGroup(donor.DeleteRoute, "route")
+
+	c.RegisterGroup(notice.CreateRoute, "route")
+	c.RegisterGroup(notice.ReadRoute, "route")
+	c.RegisterGroup(notice.UpdateRoute, "route")
+	c.RegisterGroup(notice.DeleteRoute, "route")
+
+	c.RegisterGroup(organization.CreateRoute, "route")
+	c.RegisterGroup(organization.ReadRoute, "route")
+	c.RegisterGroup(organization.UpdateRoute, "route")
+	c.RegisterGroup(organization.DeleteRoute, "route")
+
+	c.RegisterGroup(patient.CreateRoute, "route")
+	c.RegisterGroup(patient.ReadRoute, "route")
+	c.RegisterGroup(patient.UpdateRoute, "route")
+	c.RegisterGroup(patient.DeleteRoute, "route")
+
+	c.RegisterGroup(staticcontent.CreateRoute, "route")
+	c.RegisterGroup(staticcontent.ReadRoute, "route")
+	c.RegisterGroup(staticcontent.UpdateRoute, "route")
+	c.RegisterGroup(staticcontent.DeleteRoute, "route")
+
+	c.RegisterGroup(wsroute.WSRoute, "ws_route")
+}
