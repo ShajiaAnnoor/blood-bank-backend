4c7ed95718fa4870c498d9134ea894e1942c369b
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-03-08 15:42:33 +0600

    Update blood request delete

--- api/bloodrequest/delete.go
+++ api/bloodrequest/delete.go
@@ -1,112 +1,110 @@
 package bloodrequest
 
 import (
-	"fmt"
-	"time"
+	"io"
+	"net/http"
 
 	"github.com/sirupsen/logrus"
+	"gitlab.com/Aubichol/blood-bank-backend/api/middleware"
+	"gitlab.com/Aubichol/blood-bank-backend/api/routeutils"
+	"gitlab.com/Aubichol/blood-bank-backend/apipattern"
+	"gitlab.com/Aubichol/blood-bank-backend/bloodrequest"
 	"gitlab.com/Aubichol/blood-bank-backend/bloodrequest/dto"
-	"gitlab.com/Aubichol/blood-bank-backend/errors"
-	"gitlab.com/Aubichol/blood-bank-backend/model"
-	storebloodrequest "gitlab.com/Aubichol/blood-bank-backend/store/bloodrequest"
-	"gopkg.in/go-playground/validator.v9"
+	"go.uber.org/dig"
 )
 
-//Deleter provides an interface for updating bloodrequestes
-type Deleter interface {
-	Delete(*dto.Delete) (*dto.DeleteResponse, error)
+//deleteHandler holds comment update handler
+type deleteHandler struct {
+	delete bloodrequest.Updater
 }
 
-// delete deletes bloodrequest
-type delete struct {
-	storeBloodRequest storebloodrequest.BloodRequest
-	validate          *validator.Validate
-}
-
-func (u *delete) toModel(userbloodrequest *dto.Delete) (bloodrequest *model.BloodRequest) {
-	bloodrequest = &model.BloodRequest{}
-	bloodrequest.CreatedAt = time.Now().UTC()
-	bloodrequest.DeletedAt = bloodrequest.CreatedAt
-	bloodrequest.Status = userbloodrequest.Status
-	bloodrequest.UserID = userbloodrequest.UserID
-	bloodrequest.ID = userbloodrequest.StatusID
-	return
-}
-
-func (u *delete) validateData(delete *dto.Delete) (err error) {
-	err = delete.Validate(u.validate)
-	return
-}
-
-func (u *delete) convertData(delete *dto.Delete) (
-	modelBloodRequest *model.BloodRequest,
-) {
-	modelBloodRequest = u.toModel(delete)
-	return
-}
-
-func (u *delete) askStore(modelBloodRequest *model.BloodRequest) (
-	id string,
+func (ch *deleteHandler) decodeBody(
+	body io.ReadCloser,
+) (
+	bloodrequest dto.Update,
 	err error,
 ) {
-	id, err = u.storeBloodRequest.Save(modelBloodRequest)
+	err = bloodrequest.FromReader(body)
 	return
 }
 
-func (u *delete) giveResponse(
-	modelBloodRequest *model.BloodRequest,
-	id string,
-) *dto.DeleteResponse {
-	logrus.WithFields(logrus.Fields{
-		"id": modelBloodRequest.UserID,
-	}).Debug("User deleted bloodrequest successfully")
-
-	return &dto.DeleteResponse{
-		Message:    "BloodRequest deleted",
-		OK:         true,
-		ID:         id,
-		DeleteTime: modelBloodRequest.DeletedAt.String(),
-	}
-}
-
-func (u *delete) giveError() (err error) {
-	errResp := errors.Unknown{
-		Base: errors.Base{
-			OK:      false,
-			Message: "Invalid data",
-		},
-	}
-	err = fmt.Errorf(
-		"%s %w",
-		err.Error(),
-		&errResp,
-	)
-	return
-}
-
-//Delete implements Delete interface
-func (u *delete) Delete(delete *dto.Delete) (
-	*dto.DeleteResponse, error,
+func (ch *deleteHandler) handleError(
+	w http.ResponseWriter,
+	err error,
+	message string,
 ) {
-	if err := u.validateData(delete); err != nil {
-		return nil, err
-	}
-
-	modelBloodRequest := u.convertData(delete)
-	id, err := u.askStore(modelBloodRequest)
-	if err == nil {
-		return u.giveResponse(modelBloodRequest, id), nil
-	}
-
-	logrus.Error("Could not delete bloodrequest ", err)
-	err = u.giveError()
-	return nil, err
+	logrus.Error(message, err)
+	routeutils.ServeError(w, err)
 }
 
-//NewDelete returns new instance of NewCreate
-func NewDelete(store storebloodrequest.BloodRequest, validate *validator.Validate) Deleter {
-	return &delete{
-		store,
-		validate,
+func (ch *deleteHandler) decodeContext(
+	r *http.Request,
+) (userID string) {
+	userID = r.Context().Value("userID").(string)
+	return
+}
+
+func (dh *deleteHandler) askController(update *dto.Update) (
+	resp *dto.UpdateResponse,
+	err error,
+) {
+	resp, err = dh.delete.Update(update)
+	return
+}
+
+func (dh *deleteHandler) responseSuccess(
+	w http.ResponseWriter,
+	resp *dto.UpdateResponse,
+) {
+	routeutils.ServeResponse(
+		w,
+		http.StatusOK,
+		resp,
+	)
+}
+
+//ServeHTTP implements http.Handler interface
+func (dh *deleteHandler) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	defer r.Body.Close()
+
+	bloodrequest := dto.Update{}
+	bloodrequest, err := dh.decodeBody(r.Body)
+
+	if err != nil {
+		message := "Unable to decode bloodrequest delete error: "
+		dh.handleError(w, err, message)
+		return
+	}
+
+	bloodrequest.UserID = dh.decodeContext(r)
+
+	data, err := dh.askController(&bloodrequest)
+
+	if err != nil {
+		message := "Unable to delete bloodrequest error: "
+		dh.handleError(w, err, message)
+		return
+	}
+
+	dh.responseSuccess(w, data)
+}
+
+//DeleteParams provide parameters for bloodrequest delete handler
+type DeleteParams struct {
+	dig.In
+	Delete     bloodrequest.Updater
+	Middleware *middleware.Auth
+}
+
+//DeleteRoute provides a route that deletes bloodrequest
+func DeleteRoute(params DeleteParams) *routeutils.Route {
+	handler := deleteHandler{params.Delete}
+	return &routeutils.Route{
+		Method:  http.MethodPost,
+		Pattern: apipattern.BloodRequestUpdate,
+		Handler: params.Middleware.Middleware(&handler),
 	}
 }
