37232ca77a5c03c7215f2399b8bc8cf36854093a
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-03-08 17:44:09 +0600

    Update comment

--- donor/dto/count.go
+++ donor/dto/count.go
@@ -1,6 +1,6 @@
 package dto
 
-//CountByStatusReq helps gather the data for reading status by user
+//CountByDonorReq helps gather the data for reading status by user
 type CountByDonorReq struct {
 	DonorID string
 }
