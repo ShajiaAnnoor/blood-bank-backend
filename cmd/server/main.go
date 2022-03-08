c74fa2aa0901a673b142fcb3e6c31772d81c9464
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-03-08 20:23:08 +0600

    Update import paths

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"blood-bank-backend/bootstrap/server"
 	"net/http"
+
+	"gitlab.com/Aubichol/blood-bank-backend/bootstrap/server"
 
 	"github.com/sirupsen/logrus"
 	"gitlab.com/Aubichol/blood-bank-backend/bootstrap/server"
