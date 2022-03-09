dbed2c598058d452fa21f9be8a0b265876798275
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-03-09 16:09:48 +0600

    Add package for token

---
+++ store/token/mongo/model/token.go
@@ -0,0 +1,46 @@
+package model
+
+import (
+	"time"
+
+	"gitlab.com/Aubichol/hrishi-backend/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+//Token defines mongodb data type for Token
+type Token struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Token     string             `bson:"token"`
+	UserID    primitive.ObjectID `bson:"user_id"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at"`
+}
+
+//FromModel converts model data to bson data
+func (t *Token) FromModel(modelToken *model.Token) error {
+	t.Token = modelToken.Token
+	t.CreatedAt = modelToken.CreatedAt
+	t.UpdatedAt = modelToken.UpdatedAt
+
+	var err error
+
+	t.UserID, err = primitive.ObjectIDFromHex(modelToken.UserID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//ModelToken converts bson to model for token
+func (t *Token) ModelToken() *model.Token {
+	token := model.Token{}
+	token.ID = t.ID.Hex()
+	token.Token = t.Token
+	token.UserID = t.ID.Hex()
+	token.CreatedAt = t.CreatedAt
+	token.UpdatedAt = t.UpdatedAt
+
+	return &token
+}
