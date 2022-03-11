7af3a3ac929eb71f8a1a13d90c01d76538bb467e
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-03-11 13:46:35 +0600

    Add model for notice

---
+++ model/notice.go
@@ -0,0 +1,12 @@
+package model
+
+import "time"
+
+// Patient defines comment model
+type Notice struct {
+	ID        string
+	UserID    string
+	Patient   string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
