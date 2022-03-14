1076ed3c5ee9d04902e565d00ab2cdba64dfde19
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-03-14 16:40:59 +0600

    Update types

--- donor/dto/readresp.go
+++ donor/dto/readresp.go
@@ -4,12 +4,18 @@ import "gitlab.com/Aubichol/blood-bank-backend/model"
 
 //ReadResp holds the response data for reading donor
 type ReadResp struct {
-	Donor  string `json:"donor"`
-	Sender string `json:"sender"`
+	Name         string `json:"name"`
+	Phone        string `json:"phone_number"`
+	District     string `json:"district"`
+	BloodGroup   string `json:"blood_group"`
+	Address      string `json:"address"`
+	Availability bool   `json:"availability"`
+	TimesDonated int    `json:"times_donated"`
+	UserID       string `json:"user_id"`
 }
 
 //FromModel converts the model data to response data
 func (r *ReadResp) FromModel(donor *model.Donor) {
 	//	r.Donor = donor.Donor
-	r.Sender = donor.UserID
+	//r.Sender = donor.UserID
 }
