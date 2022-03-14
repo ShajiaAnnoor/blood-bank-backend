4511779cfc5f71604159425826624ccadaafd230
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-03-14 22:53:20 +0600

    Update data type for notice response

--- notice/dto/readresp.go
+++ notice/dto/readresp.go
@@ -4,12 +4,17 @@ import "gitlab.com/Aubichol/blood-bank-backend/model"
 
 //ReadResp holds the response data for reading notice
 type ReadResp struct {
-	Notice string `json:"notice"`
-	Sender string `json:"sender"`
+	PatientName string `json:"patient_name"`
+	BloodGroup  string `json:"notice_group"`
+	Description string `json:"description"`
+	District    string `json:"district"`
+	Address     string `json:"address"`
+	Title       string `json:"title"`
+	UserID      string `json:"user_id"`
 }
 
 //FromModel converts the model data to response data
 func (r *ReadResp) FromModel(notice *model.Notice) {
-	r.Notice = notice.Notice
-	r.Sender = notice.UserID
+	//r.Notice = notice.Notice
+	//r.Sender = notice.UserID
 }
