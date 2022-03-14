c959d8d73970d0b72f1c1d2cae06ba54c7820747
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-03-14 22:50:39 +0600

    Update blood request update data type

--- bloodrequest/dto/readresp.go
+++ bloodrequest/dto/readresp.go
@@ -4,12 +4,13 @@ import "gitlab.com/Aubichol/blood-bank-backend/model"
 
 //ReadResp holds the response data for reading blood request
 type ReadResp struct {
-	Request string `json:"request"`
-	Sender  string `json:"sender"`
+	Request    string `json:"request"`
+	BloodGroup string `json:"blood_group"`
+	UserID     string `json:"user_id"`
 }
 
 //FromModel converts the model data to response data
 func (r *ReadResp) FromModel(bloodreq *model.BloodRequest) {
 	r.Request = bloodreq.Request
-	r.Sender = bloodreq.UserID
+	//	r.Sender = bloodreq.UserID
 }
