a2ab7a9d5876ae90506416950dc9c7af94195734
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-03-14 23:06:01 +0600

    Update data type

--- patient/dto/readresp.go
+++ patient/dto/readresp.go
@@ -4,8 +4,14 @@ import "gitlab.com/Aubichol/blood-bank-backend/model"
 
 //ReadResp holds the response data for reading patient
 type ReadResp struct {
-	Patient string `json:"patient"`
-	Sender  string `json:"sender"`
+	Patient    string `json:"patient"`
+	Sender     string `json:"sender"`
+	Name       string `json:"name"`
+	BloodGroup string `json:"blood_group"`
+	District   string `json:"district"`
+	Phone      string `json:"phone_number"`
+	Address    string `json:"address"`
+	UserID     string `json:"user_id"`
 }
 
 //FromModel converts the model data to response data
