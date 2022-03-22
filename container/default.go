10a6cc5b38df523339826b9115b572f3ea6725f0
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-03-22 15:14:45 +0600

    Refactor names

--- container/default.go
+++ container/default.go
@@ -11,29 +11,29 @@ type defaultContainer struct {
 }
 
 //Register registers a provider
-func (d *defaultContainer) Register(provider interface{}) {
-	if err := d.Container.Provide(provider); err != nil {
+func (dc *defaultContainer) Register(provider interface{}) {
+	if err := dc.Container.Provide(provider); err != nil {
 		logrus.Fatal(err)
 	}
 }
 
 //RegisterWithName registers a provider with name.
 //This scenario is helpful when multiple providers give same service
-func (d *defaultContainer) RegisterWithName(provider interface{}, containerName string) {
-	if err := d.Container.Provide(provider, dig.Name(containerName)); err != nil {
+func (dc *defaultContainer) RegisterWithName(provider interface{}, containerName string) {
+	if err := dc.Container.Provide(provider, dig.Name(containerName)); err != nil {
 		logrus.Fatal(err)
 	}
 }
 
-func (d *defaultContainer) Resolve(function interface{}) {
-	if err := d.Invoke(function); err != nil {
+func (dc *defaultContainer) Resolve(function interface{}) {
+	if err := dc.Invoke(function); err != nil {
 		logrus.Fatal(err)
 	}
 }
 
 //RegisterGroup registers a provider that belongs to a group
-func (d *defaultContainer) RegisterGroup(provider interface{}, name string) {
-	if err := d.Provide(provider, dig.Group(name)); err != nil {
+func (dc *defaultContainer) RegisterGroup(provider interface{}, name string) {
+	if err := dc.Provide(provider, dig.Group(name)); err != nil {
 		logrus.Fatal(err)
 	}
 }
