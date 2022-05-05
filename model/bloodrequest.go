594fd43f9a92b33a71849a955d616e0b3ccfe92e
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-05-05 19:02:54 +0600

    Update format

--- model/bloodrequest.go
+++ model/bloodrequest.go
@@ -4,10 +4,11 @@ import "time"
 
 // BloodRequest defines blood request model
 type BloodRequest struct {
-	ID        string
-	UserID    string
-	Patient   string
-	Request   string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID         string
+	UserID     string
+	Patient    string
+	Request    string
+	BloodGroup string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
 }
