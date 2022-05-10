7313851d8de22518e47532b57653aaed8d10dffb
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-05-11 02:35:24 +0600

    Update patient data for store

--- store/patient/mongo/model/patient.go
+++ store/patient/mongo/model/patient.go
@@ -9,42 +9,46 @@ import (
 
 //Patient holds db data type for patients
 type Patient struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	Comment   string             `bson:"comment"`
-	UserID    primitive.ObjectID `bson:"user_id"`
-	StatusID  primitive.ObjectID `bson:"status_id"`
-	CreatedAt time.Time          `bson:"created_at"`
-	UpdatedAt time.Time          `bson:"updated_at"`
+	ID         primitive.ObjectID `bson:"_id,omitempty"`
+	Name       string             `bson:"name"`
+	BloodGroup string             `bson:"blood_group"`
+	District   string             `bson:"district"`
+	Phone      string             `bson:"phone_number"`
+	Address    string             `bson:"address"`
+	UserID     primitive.ObjectID `bson:"user_id,omitempty"`
+	CreatedAt  time.Time          `bson:"created_at"`
+	UpdatedAt  time.Time          `bson:"updated_at"`
 }
 
 //FromModel converts model data to db data for comments
 func (p *Patient) FromModel(modelPatient *model.Patient) error {
-	p.Comment = modelPatient.Patient
 	p.CreatedAt = modelPatient.CreatedAt
 	p.UpdatedAt = modelPatient.UpdatedAt
+	p.Name = modelPatient.Name
+	p.Address = modelPatient.Address
+	p.District = modelPatient.District
+	p.Phone = modelPatient.Phone
+	p.Address = modelPatient.Address
+	p.BloodGroup = modelPatient.BloodGroup
 
 	var err error
-	p.ID, err = primitive.ObjectIDFromHex(modelPatient.ID)
+
+	if modelPatient.ID != "" {
+		p.ID, err = primitive.ObjectIDFromHex(modelPatient.ID)
+	}
 
 	if err != nil {
 		return err
 	}
 
-	p.UserID, err = primitive.ObjectIDFromHex(modelPatient.UserID)
+	if modelPatient.UserID != "" {
+		p.UserID, err = primitive.ObjectIDFromHex(modelPatient.UserID)
+	}
+
 	if err != nil {
 		return err
 	}
 
-	if modelPatient.ID == "" {
-		return nil
-	}
-
-	id, err := primitive.ObjectIDFromHex(modelPatient.ID)
-	if err != nil {
-		return err
-	}
-
-	p.ID = id
 	return nil
 }
 
@@ -52,11 +56,15 @@ func (p *Patient) FromModel(modelPatient *model.Patient) error {
 func (p *Patient) ModelPatient() *model.Patient {
 	patient := model.Patient{}
 	patient.ID = p.ID.Hex()
-	patient.Patient = p.Comment
 	patient.UserID = p.UserID.Hex()
-	//	patient.StatusID = p.StatusID.Hex()
 	patient.CreatedAt = p.CreatedAt
 	patient.UpdatedAt = p.UpdatedAt
+	patient.Address = p.Address
+	patient.Name = p.Name
+	patient.BloodGroup = p.BloodGroup
+	patient.District = p.District
+	patient.Phone = p.Phone
+	patient.Address = p.Address
 
 	return &patient
 }
