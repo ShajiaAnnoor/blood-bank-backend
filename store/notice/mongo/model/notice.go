ca30eb0c6bd70b9425c03027298e90fe4029d51c
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-05-12 20:11:17 +0600

    Update db logic for storing notices

--- store/notice/mongo/model/notice.go
+++ store/notice/mongo/model/notice.go
@@ -9,28 +9,37 @@ import (
 
 //Notice holds db data type for notices
 type Notice struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	Notice    string             `bson:"notice"`
-	UserID    primitive.ObjectID `bson:"user_id"`
-	NoticeID  primitive.ObjectID `bson:"status_id"`
-	CreatedAt time.Time          `bson:"created_at"`
-	UpdatedAt time.Time          `bson:"updated_at"`
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	BloodGroup  string             `bson:"blood_group,omitempty"`
+	Description string             `bson:"description"`
+	Title       string             `bson:"title"`
+	UserID      primitive.ObjectID `bson:"user_id"`
+	CreatedAt   time.Time          `bson:"created_at"`
+	UpdatedAt   time.Time          `bson:"updated_at"`
 }
 
 //FromModel converts model data to db data for notices
 func (n *Notice) FromModel(modelNotice *model.Notice) error {
-	n.Notice = modelNotice.Notice
 	n.CreatedAt = modelNotice.CreatedAt
 	n.UpdatedAt = modelNotice.UpdatedAt
+	n.BloodGroup = modelNotice.BloodGroup
+	n.Description = modelNotice.Description
+	n.Title = modelNotice.Title
 
 	var err error
-	n.ID, err = primitive.ObjectIDFromHex(modelNotice.ID)
+
+	if modelNotice.ID != "" {
+		n.ID, err = primitive.ObjectIDFromHex(modelNotice.ID)
+	}
 
 	if err != nil {
 		return err
 	}
 
-	n.UserID, err = primitive.ObjectIDFromHex(modelNotice.UserID)
+	if modelNotice.UserID != "" {
+		n.UserID, err = primitive.ObjectIDFromHex(modelNotice.UserID)
+	}
+
 	if err != nil {
 		return err
 	}
@@ -52,9 +61,7 @@ func (n *Notice) FromModel(modelNotice *model.Notice) error {
 func (n *Notice) ModelNotice() *model.Notice {
 	notice := model.Notice{}
 	notice.ID = n.ID.Hex()
-	notice.Notice = n.Notice
 	notice.UserID = n.UserID.Hex()
-	//	notice.StatusID = n.StatusID.Hex()
 	notice.CreatedAt = n.CreatedAt
 	notice.UpdatedAt = n.UpdatedAt
 
