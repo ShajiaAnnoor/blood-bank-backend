d27a0539685e5c00a2ae5df484889e8192c512cb
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-05-13 00:38:08 +0600

    Assign data for responding to a notice

--- notice/dto/readresp.go
+++ notice/dto/readresp.go
@@ -15,6 +15,11 @@ type ReadResp struct {
 
 //FromModel converts the model data to response data
 func (r *ReadResp) FromModel(notice *model.Notice) {
-	//r.Notice = notice.Notice
-	//r.Sender = notice.UserID
+	r.PatientName = notice.PatientName
+	r.BloodGroup = notice.BloodGroup
+	r.Description = notice.Description
+	r.District = notice.District
+	r.Address = notice.Address
+	r.Title = notice.Title
+	r.UserID = notice.UserID
 }
