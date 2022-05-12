2a841ac9ce5c65064a6df0db72ecc4d7a024100d
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-05-12 16:59:02 +0600

    Update model for static content

--- model/staticcontent.go
+++ model/staticcontent.go
@@ -4,9 +4,9 @@ import "time"
 
 // StaticContent defines static content model
 type StaticContent struct {
-	ID            string
-	UserID        string
-	StaticContent string
-	CreatedAt     time.Time
-	UpdatedAt     time.Time
+	ID        string
+	UserID    string
+	Text      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
