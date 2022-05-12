0a50c208afb61cd1e563d72a9f4b3dc6517517ef
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-05-12 16:34:07 +0600

    Update organization data for data store

--- store/organization/mongo/model/organization.go
+++ store/organization/mongo/model/organization.go
@@ -9,54 +9,59 @@ import (
 
 //Comment holds db data type for comments
 type Organization struct {
-	ID           primitive.ObjectID `bson:"_id,omitempty"`
-	Organization string             `bson:"organization"`
-	UserID       primitive.ObjectID `bson:"user_id"`
-	StatusID     primitive.ObjectID `bson:"status_id"`
-	CreatedAt    time.Time          `bson:"created_at"`
-	UpdatedAt    time.Time          `bson:"updated_at"`
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	Description string             `bson:"description,omitempty"`
+	Phone       string             `bson:"phone_number,omitempty"`
+	Name        string             `bson:"name,omitempty"`
+	Address     string             `bson:"address,omitempty"`
+	District    string             `bson:"district,omitempty"`
+	UserID      primitive.ObjectID `bson:"user_id,omitempty"`
+	CreatedAt   time.Time          `bson:"created_at,omitempty"`
+	UpdatedAt   time.Time          `bson:"updated_at,omitempty"`
 }
 
 //FromModel converts model data to db data for comments
 func (o *Organization) FromModel(modelOrganization *model.Organization) error {
-	o.Organization = modelOrganization.Organization
 	o.CreatedAt = modelOrganization.CreatedAt
 	o.UpdatedAt = modelOrganization.UpdatedAt
+	o.Name = modelOrganization.Name
+	o.Address = modelOrganization.Address
+	o.Description = modelOrganization.Description
+	o.Phone = modelOrganization.Phone
+	o.District = modelOrganization.District
 
 	var err error
-	o.ID, err = primitive.ObjectIDFromHex(modelOrganization.ID)
+
+	if modelOrganization.ID != "" {
+		o.ID, err = primitive.ObjectIDFromHex(modelOrganization.ID)
+	}
 
 	if err != nil {
 		return err
 	}
 
-	o.UserID, err = primitive.ObjectIDFromHex(modelOrganization.UserID)
+	if modelOrganization.UserID != "" {
+		o.UserID, err = primitive.ObjectIDFromHex(modelOrganization.UserID)
+	}
+
 	if err != nil {
 		return err
 	}
 
-	if modelOrganization.ID == "" {
-		return nil
-	}
-
-	id, err := primitive.ObjectIDFromHex(modelOrganization.ID)
-	if err != nil {
-		return err
-	}
-
-	o.ID = id
 	return nil
 }
 
 //ModelOrganization converts bson to model
 func (o *Organization) ModelOrganization() *model.Organization {
 	organization := model.Organization{}
-	organization.ID = o.ID.Hex()
-	//	organization.Comment = o.Organization
-	organization.UserID = o.UserID.Hex()
-	//	organization.StatusID = o.StatusID.Hex()
+	//	organization.ID = o.ID.Hex()
+	//	organization.UserID = o.UserID.Hex()
 	organization.CreatedAt = o.CreatedAt
 	organization.UpdatedAt = o.UpdatedAt
+	organization.Name = o.Name
+	organization.Address = o.Address
+	organization.Description = o.Description
+	organization.Phone = o.Phone
 
 	return &organization
 }
