728dad131a7ef91b7a8c306d2e9d030ab996d19c
Tarif Ezaz <tarif.ezaz@gmail.com>
2022-05-13 02:02:03 +0600

    Assign response data for organization

--- organization/dto/readresp.go
+++ organization/dto/readresp.go
@@ -4,16 +4,20 @@ import "gitlab.com/Aubichol/blood-bank-backend/model"
 
 //ReadResp holds the response data for reading organization
 type ReadResp struct {
-	Organization string `json:"organization"`
-	Name         string `json:"name"`
-	Phone        string `json:"phone_number"`
-	District     string `json:"district"`
-	Description  string `json:"description"`
-	Address      string `json:"address"`
-	UserID       string `json:"user_id"`
+	Name        string `json:"name"`
+	Phone       string `json:"phone_number"`
+	District    string `json:"district"`
+	Description string `json:"description"`
+	Address     string `json:"address"`
+	UserID      string `json:"user_id"`
 }
 
 //FromModel converts the model data to response data
 func (r *ReadResp) FromModel(org *model.Organization) {
-	r.Organization = org.Organization
+	r.Name = org.Name
+	r.Phone = org.Phone
+	r.District = org.District
+	r.Description = org.Description
+	r.Address = org.Address
+	r.UserID = org.UserID
 }
